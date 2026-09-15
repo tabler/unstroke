2b394816d9d4d787c36fa2f8afc63d02f27a10d8
codecalm <codecalm@gmail.com>
2026-09-15 13:31:36 +0200

    Build ESM and CJS bundles with tsup; add exports map

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'tsup';
+
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['esm'],
+  format: ['esm', 'cjs'],
   dts: true,
   sourcemap: true,
   clean: true,
